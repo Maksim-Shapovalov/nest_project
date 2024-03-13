80e3fa24f46a441d85b10b2317745c423ee39767
Maksim Shapovalov <maksim.shapovalov.01@gmail.com>
2024-03-13 20:38:55 +0300

    13

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -56,7 +56,7 @@ export class AuthController {
       httpOnly: true,
       secure: true,
     });
-    return accessToken;
+    return { accessToken };
   }
   @UseGuards(AuthGuard)
   @Post('refresh-token')
