5a592a9923e6f92c5ce41483b8a5fa35d215953b
Maksim Shapovalov <maksim.shapovalov.01@gmail.com>
2024-03-20 19:55:00 +0300

    21

--- src/exception.filter.ts
+++ src/exception.filter.ts
@@ -19,13 +19,11 @@ export class HttpExceptionFilter implements ExceptionFilter {
         errorsMessages: [],
       };
       const responseBody: any = exception.getResponse();
-      if (typeof responseBody.message === 'string') {
-        errorResponse.errorsMessages.push(responseBody.message);
-      } else {
-        responseBody.message.forEach((m) =>
-          errorResponse.errorsMessages.push(m),
-        );
-      }
+      // if (typeof responseBody.message === 'string') {
+      //   errorResponse.errorsMessages.push(responseBody.message);
+      // } else {
+      responseBody.message.forEach((m) => errorResponse.errorsMessages.push(m));
+      // }
       response.status(status).json(errorResponse);
     } else if (status == 401) {
       response.sendStatus(status);
