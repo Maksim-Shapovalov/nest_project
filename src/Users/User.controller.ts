8fee103c415649e1e5dc657f65f12f2663971d52
Maksim Shapovalov <maksim.shapovalov.01@gmail.com>
2024-05-11 15:39:01 +0300

    29

--- src/Users/User.controller.ts
+++ src/Users/User.controller.ts
@@ -48,6 +48,7 @@ export class UserController {
   @Post()
   @HttpCode(201)
   async createNewUser(@Body() inputModel: UserBasicRequestBody) {
+    console.log('i');
     const user = {
       login: inputModel.login,
       password: inputModel.password,
