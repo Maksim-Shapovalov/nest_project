a94472b382ad52453f025c51cb370fb9debacd48
Maksim Shapovalov <maksim.shapovalov.01@gmail.com>
2024-11-13 23:51:25 +0300

    refresh_2

--- src/quiz/QuizGame.controller.ts
+++ src/quiz/QuizGame.controller.ts
@@ -32,7 +32,6 @@ export class QuizGameController {
     @User() userModel: NewestPostLike,
     @Query() query: QueryType2,
   ) {
-    console.log(1);
     const filter = queryFilterByQuizGame(query);
     const findHistoryGameByPlayer =
       await this.quizGameService.getHistoryGameByPlayerService(
