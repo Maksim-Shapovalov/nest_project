0c10bce838fb8800af04f0b6269a7cb94074d5b6
Максим <maksim.shapovalov.01@gmail.com>
2025-04-25 14:37:03 +0300

    add banned_7

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ const serverUrl = 'http://localhost:3000';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   appSettings(app);
+  console.log(3000);
   await app.listen(3000);
 }
 bootstrap();
